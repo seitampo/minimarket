b1c5eee92559074ca496083d6176684a483c764e
assanali <145455102+seitampo@users.noreply.github.com>
2025-06-19 17:54:12 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MiniMarket PVL</h1>
+      <p>Добро пожаловать! Здесь будет ваш интернет-магазин.</p>
+    </div>
+  );
+}
+
+export default App;
